cdd8e635e64b73fa5d8ecce0100f2e6b699980b8
Nick Jones <nickjones9605@gmail.com>
2022-06-13 19:01:05 -0600

    Feat: added multiple methods to encrypt, shift characters, reverse shift characters, and decrypt a message

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,7 +8,63 @@ class Enigma
 
   def encrypt(message, key, date)
     encryption_generator = EncryptionGenerators.new
-    encryption_generator.generate_shifts(date, key)
+    shift_array = encryption_generator.generate_shifts(date, key)
+    shifts_index = 0
+    encrypted_message = []
+    message.each_char do |character|
+      encrypted_character = shift_character(character, shift_array[shifts_index])
+      shifts_index += 1
+      if shifts_index > 3
+        shifts_index = 0
+      end
+      encrypted_message << encrypted_character
+    end
+    encryption_hash = {}
+    encryption_hash[:key] = key
+    encryption_hash[:date] = date
+    encryption_hash[:encryption] = encrypted_message.join
+    return encryption_hash
   end
 
+  def shift_character(character, shift_value)
+    character = character.downcase
+    index_number = @character_set.index(character)
+    shifted_index = index_number + shift_value
+    while shifted_index > (@character_set.length - 1)
+      shifted_index -= (@character_set.length)
+    end
+    shifted_character = @character_set[shifted_index]
+    return shifted_character
+  end
+
+  def reverse_shift_character(character, shift_value)
+    character = character.downcase
+    index_number = @character_set.index(character)
+    shifted_index = index_number - shift_value
+    while shifted_index < 0
+      shifted_index += (@character_set.length)
+    end
+    shifted_character = @character_set[shifted_index]
+    return shifted_character
+  end
+
+  def decrypt(encrypted_message, key, date)
+    encryption_generator = EncryptionGenerators.new
+    shift_array = encryption_generator.generate_shifts(date, key)
+    shifts_index = 0
+    decrypted_message = []
+    encrypted_message.each_char do |character|
+      decrypted_character = reverse_shift_character(character, shift_array[shifts_index])
+      shifts_index += 1
+      if shifts_index > 3
+        shifts_index = 0
+      end
+      decrypted_message << decrypted_character
+    end
+    decryption_hash = {}
+    decryption_hash[:key] = key
+    decryption_hash[:date] = date
+    decryption_hash[:decryption] = decrypted_message.join
+    return decryption_hash
+  end
 end
