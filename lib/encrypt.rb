9f516e02c35043664475a63c38581a55f31f5c0a
Nick Jones <nickjones9605@gmail.com>
2022-06-14 16:58:33 -0600

    Feat: changed comments

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,3 +7,9 @@ message_text = ARGV[0]
 encrypted_text = ARGV[1]
 
 message = File.new(message_text).read
+
+ile.open(encrypted_text, "w") do |text_file|
+  enigma.encrypt(message) # reads what is inside message text and passes through to enigma.rb
+  text_file.puts enigma.encrypted_hash[:encryption]
+  puts "Created #{encrypted_text} with the key #{enigma.encrypted_hash[:key]} and date #{enigma.encrypted_hash[:date]}."
+end
