e6aec877c255f933f368fec1536f7564abcee1ee
Nick Jones <nickjones9605@gmail.com>
2022-06-14 17:09:42 -0600

    add text file puts decrypted hash

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -9,3 +9,5 @@ decrypted_text = ARGV[1]
 message = File.new(message_text).read
 
 File.open(decrypted_text, "w") do |text_file|
+  enigma.decrypt(message)
+  text_file.puts enigma.decrypted_hash[:decryption]
