c21380f17e6352c16177bec15be65e1faa37a3fa
Nick Jones <nickjones9605@gmail.com>
2022-06-13 18:58:55 -0600

    Test: add test for shifting a character and decrypting a message

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,10 +8,25 @@ RSpec.describe Enigma do
   expect(enigma).to be_instance_of(Enigma)
   end
 
-  xit "can return an encryption hash" do
+  it "can return an encryption hash" do
     enigma = Enigma.new
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq({
     encryption: "keder ohulw",
+    key: "02715",
+    date: "040895"})
+  end
+
+  it "can shift a character" do
+    enigma = Enigma.new
+    expect(enigma.shift_character("A", 5)).to eq("f")
+    expect(enigma.shift_character("Z", 2)).to eq("a")
+    expect(enigma.shift_character("Z", 29)).to eq("a")
+  end
+
+  it "can decrypt a message" do
+    enigma = Enigma.new
+    expect(enigma.decrypt("keder ohulw","02715","040895")).to eq({
+    decryption: "hello world",
     key: "02715",
     date: "040895"})
   end
