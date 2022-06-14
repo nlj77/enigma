01b5a11bbeb9a78d64292aba9bafd21cc3fda1d5
Nick Jones <nickjones9605@gmail.com>
2022-06-13 19:00:27 -0600

    Feat: updated keys to return arrays, add method to generate shifts

--- lib/encryption_generators.rb
+++ lib/encryption_generators.rb
@@ -1,18 +1,25 @@
 require 'date'
 class EncryptionGenerators
 
-  def generate_key
-    new_seed = (0..5).to_a.sample(5).join
-    keys = Hash.new
-    keys["A"] = new_seed[0] + new_seed[1]
-    keys["B"] = new_seed[1] + new_seed[2]
-    keys["C"] = new_seed[2] + new_seed[4]
-    keys["D"] = new_seed[3] + new_seed[4]
-    return keys
+  def generate_key(seed = nil)
+    if seed == nil
+      seed= (0..5).to_a.sample(5).join
+    end
+    # key = Hash.new
+    # keys["A"] = new_seed[0] + new_seed[1]
+    # keys["B"] = new_seed[1] + new_seed[2]
+    # keys["C"] = new_seed[2] + new_seed[4]
+    # keys["D"] = new_seed[3] + new_seed[4]
+    key = []
+    key[0] = seed[0] + seed[1]
+    key[1] = seed[1] + seed[2]
+    key[2] = seed[2] + seed[3]
+    key[3] = seed[3] + seed[4]
+    return key
   end
 
   def generate_offsets(date)
-    date_squared = date * date
+    date_squared = date.to_i * date.to_i
     date_squared = date_squared.to_s
     new_offsets = date_squared[-4..-1]
     offsets = Hash.new
@@ -23,11 +30,15 @@ class EncryptionGenerators
     return offsets
   end
 
-  def generate_shifts(  date = Date.today.strftime("%-m%d%y"), key = generate_key)
+  def generate_shifts(  date = Date.today.strftime("%-m%d%y"), key_seed)
     offsets = generate_offsets(date)
     offsets.transform_values!(&:to_i)
-    key.transform_values!(&:to_i)
-    shift_array = offsets.values.map.with_index {|v, i| v + key.values[i]}
+    shift_array = []
+    key = generate_key(key_seed)
+    shift_array[0] = key[0].to_i + offsets.values[0].to_i
+    shift_array[1] = key[1].to_i + offsets.values[1].to_i
+    shift_array[2] = key[2].to_i + offsets.values[2].to_i
+    shift_array[3] = key[3].to_i + offsets.values[3].to_i
     return shift_array
   end
 end
